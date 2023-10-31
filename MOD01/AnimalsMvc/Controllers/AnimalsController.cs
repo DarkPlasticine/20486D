09653da3e05be1a96aac74ecdd271db87d5eb8f5
DarkPlasticine <marshalvasil@yandex.ru>
2023-10-31 13:06:19 +0700

    Добавьте файлы проекта.

---
+++ MOD01/AnimalsMvc/Controllers/AnimalsController.cs
@@ -0,0 +1,35 @@
+﻿using AnimalsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalsMvc.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private IData _tempData { get; set; }
+
+        public AnimalsController(IData tempData)
+        {
+            _tempData = tempData;
+        }
+
+        public IActionResult Index()
+        {
+            var animals = _tempData.AnimalsInitializeData();
+            var indexViewModel = new IndexViewModel();
+            indexViewModel.Animals = animals;
+            return View(indexViewModel);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var model = _tempData.GetAnimalById(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ MOD01/CakeStoreApi/Controllers/CakeStoreApiController.cs
@@ -0,0 +1,37 @@
+﻿using CakeStoreApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CakeStoreApi.Controllers
+{
+    public class CakeStoreApiController : Controller
+    {
+        private IData _data { get; set; }
+
+        public CakeStoreApiController(IData data) 
+        {
+            _data = data;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+
+        [HttpGet("/api/CakeStore/{id}", Name ="GetCake")]
+        public ActionResult<CakeStore> GetById(int? id)
+        {
+            var item = _data.GetCakeById(id);
+
+            if (item == null) { return NotFound(); }
+
+            return new ObjectResult(item);
+        }
+    }
+}
