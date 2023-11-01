74b22ac1eeb85ec156010db6cfd0a2f6c97c8308
DarkPlasticine <marshalvasil@yandex.ru>
2023-11-01 14:49:44 +0700

    Лабораторная работа модуля 5

---
+++ MOD05/01_CitiesWebsite_begin/CitiesWebsite/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using CitiesWebsite.Models;
+using CitiesWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CitiesWebsite.ViewComponents
+{
+    public class CityViewComponent: ViewComponent
+    {
+        private ICityProvider _cities;
+
+        public CityViewComponent(ICityProvider cities) {
+            _cities = cities;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string cityName)
+        {
+            ViewBag.CurrentCity = await GetCityAsync(cityName);
+            return View("SelectCity");
+        }
+
+        private Task<City> GetCityAsync(string cityName)
+        {
+            return Task.FromResult<City>(_cities[cityName]);
+        }
+    }
+}
